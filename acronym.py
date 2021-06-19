caf3ed48f4d343677681e1ecba14896d62a3cfad
heba-ali2030 <heba.galaa@gmail.com>
2021-06-19 18:16:41 +0200

    task is completed

--- acronym.py
+++ acronym.py
@@ -5,17 +5,19 @@ print(f'Hi, Tell me the full name and I will surprise you with  the Acronym ^_^'
 full_name = input('please enter the full meaning of the organization or concept ?\n')
 
 # convert the input into list
-full_name_list = full_name.split()
-print(full_name_list)
+full_name_new = full_name.split(' ')
+
 
 # iterate over the string list and print the first letter of each word
 
-for word in full_name_list:
-    first_character = word[0]
-        
+first_word = full_name_new[:-1]
+last_word = full_name_new [-1]
+acronym = ''
+for letter in first_word:
+    acronym = acronym + letter[0] 
+acronym = acronym + last_word[0]
 
 # print the result in uppercase form and tell it to the user 
-Acronym = ''. join(first_character)
-print(Acronym)
+print(F'The Acronym of {full_name} is {acronym.upper()}')
 
-# ask the user to run it again or exit
+
